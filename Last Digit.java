50ded3fec910ffc37328fea7972239482a5cf974
Sai Nikhil Varada <varadasainikhil@gmail.com>
2022-02-10 13:58:34 +0530

    Update Last Digit.java
    Shortened the code

--- Last Digit.java
+++ Last Digit.java
@@ -1,33 +1,21 @@
-public class LastDigitChecker {
-    public static boolean hasSameLastDigit(int n1,int n2,int n3) {
-        if((10<=n1)&&(n1<=1000)&&(10<=n2)&&(n2<=1000)&&(10<=n3)&&(n3<=1000)){
-            int a1 = n1%10;
-            int a2 = n2%10;
-            int a3 = n3%10;
-            if((a1==a2)||(a1==a3)||(a2==a3)){
-                System.out.println("true");
-                return true;
-            }
-            else{
-                System.out.println("false");
-                return false;
-            }
-        }
-        else{
-            System.out.println("false");
+public class LastDigitChecker{
+    public static boolean hasSameLastDigit(int n1,int n2, int n3) {
+        if (n1<10 || n1>1000 || n2<10 || n2>1000 || n3<10 || n3>1000) {
             return false;
         }
-        
-    }
-    public static boolean isValid(int number) {
-        if((number>=10)&&(number<=1000)){
-            System.out.println("true");
+        int n1lastDigit = n1 %10;
+        int n2lastDigit = n2 %10;
+        int n3lastDigit = n3 %10;
+        if (n1lastDigit == n2lastDigit || n1lastDigit == n3lastDigit || n2lastDigit == n3lastDigit) {
             return true;
         }
-        else{
-            System.out.println("false");
-            return false;
-        }
+        return false;
     }
-    
+
+    public static boolean isValid(int number) {
+        if (number >=10 && number <=1000) {
+            return true;
+        }
+        return false;
+    }
 }
