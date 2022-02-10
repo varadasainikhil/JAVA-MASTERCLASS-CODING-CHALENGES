37fe4dde8ecd4917c3e8a3dd2424c073d6d1d913
Sai Nikhil Varada <varadasainikhil@gmail.com>
2022-02-10 14:55:13 +0530

    Update Perfect Number.java
    Shortened the code

--- Perfect Number.java
+++ Perfect Number.java
@@ -1,34 +1,17 @@
 public class PerfectNumber {
     public static boolean isPerfectNumber(int number) {
-        //invalid case
-        if(number&lt;1){
-            System.out.println("Invalid input");
+        int sum=0;
+        if (number<1) {
             return false;
         }
-        //for the factors
-        int divisor = 1;
-        int sum = 0; 
-        while (number &gt; divisor) {
-            if (number % divisor == 0) {
-                System.out.println(divisor);
-                sum += divisor; 
-                divisor++;               
+        for (int i = 1; i < number ; i++) {
+            if (number%i ==0) {
+                sum+=i;
             }
-            else{
-                divisor++;
-            }
-            
         }
-        System.out.println(sum);
-        if(number == sum){
-            System.out.println("It is a perfect number");
+        if (sum == number) {
             return true;
         }
-        else{
-            System.out.println("It is not a perfect number");
-            return false;
-        }
-        
+        return false;
     }
-    
 }
